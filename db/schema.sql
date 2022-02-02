340ba383a1c87fb48f1e737d020b0d20edb0a31e
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-01 18:03:32 -0800

    add database and tables

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+
+USE business_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30),
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT,
+);
+
+CREATE TABLE employee (
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT,
+);
