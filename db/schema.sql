e45660aa48a18a5a641f23c741577719b4c5b5a2
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-02 17:49:42 -0800

    set table relationship references

--- db/schema.sql
+++ db/schema.sql
@@ -13,6 +13,9 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL, 
     department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -20,4 +23,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT, 
     manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );
