2245d41d8fd69bfae8b213360c8a2d39367f46ba
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-02 18:13:47 -0800

    set up port and middleware

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+//Import requirements and their consts
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
+//set port and express as app
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 
+
+//Set app listening on port
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
