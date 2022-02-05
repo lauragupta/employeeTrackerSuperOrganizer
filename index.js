fa2ce1a05b4b9cc7f5d7a6686bf23d9bde1780bc
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-05 15:42:48 -0800

    clean up code and add more comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const { all } = require('express/lib/application');
-// const { table } = require('console');
+
 
 // Connect to database
 const db = mysql.createConnection(
@@ -20,7 +20,6 @@ const departmentList = `SELECT * FROM department;`;
 const roleList = `SELECT * FROM role;`;
 const managerList = `SELECT id, first_name, last_name, manager_id FROM employee WHERE manager_id IS null;`;
 const employeeList = `SELECT * FROM employee ORDER BY employee.last_name;`;
-const listDepartments = `SELECT name FROM department;`;
 const allEmployeeFirstName = `SELECT id, first_name, last_name FROM employee;`;
 
 //Starting question
@@ -42,33 +41,35 @@ const addDepeartmentQuestion = [
     }
 ];
 
-
-
 //Start Inquirer and next step function
 function askStartQuestion() {
     inquirer.prompt(startQuestion).then((response) => {
+        //Show employee table
         if(response.startQuestion === 'View All Employees') {
             db.query(employeeList, function (err, results) {
                console.table(results);
                 askStartQuestion();
             });
+        //Show Department Table
         } else if(response.startQuestion === 'View All Departments') {
             db.query(departmentList, function (err, results) {
                 console.table(results); 
                 askStartQuestion();
             });
+        //Add a Department function
         } else if(response.startQuestion === 'Add Depeartment') {
             inquirer.prompt(addDepeartmentQuestion).then((response) => {
                 const newDepartment = response.department;
                 console.log(newDepartment);
                 db.query(`INSERT INTO department (name) VALUES ('${newDepartment}');`, function(err, results) {
-                    console.log(results);
+                    console.log("Success!", results);
                 });
                 askStartQuestion();
             });
+        //Add a Role function
         } else if(response.startQuestion === 'Add Role') {
             db.query(departmentList, function (err, results) {
-                //Add a Role
+                //Add a Role Questions
                 const addRoleQuestions = [
                     {
                         name: 'role',
@@ -87,18 +88,20 @@ function askStartQuestion() {
                         choices: results
                     }
                 ];
+                //Add a Role to the table
                 inquirer.prompt(addRoleQuestions).then((response) => {
                     let newRoleDepartment = response.roleDepartment;
                     db.query(`SELECT id FROM department WHERE name = '${newRoleDepartment}';`, function (err, results) {
                         console.log(results);
                         const deptID = results[0].id;
                         db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${response.role}', '${response.roleSalary}', '${deptID}');`, function(err, results) {
-                            console.log(results);
+                            console.log("Success!", results);
                             askStartQuestion();
                         });
                     });
                 });
             });
+        //Add Employee Function
         } else if(response.startQuestion === 'Add Employee') {
             db.query(roleList, function (err, roleResponse) {
                 let roleTitles = roleResponse.map(function(roleResponseItem) {
@@ -109,16 +112,14 @@ function askStartQuestion() {
                     return newRoleID;
                 })
                 db.query(managerList, function (err, managerResults) {
-                    console.log(managerResults);
                     let managers = managerResults.map(function(row) {
                         let newManagers = {
                             id: row.id,
                             name: row.first_name
                         }
-                        console.log(`managers `, newManagers);
                         return newManagers;
                     })
-                    //Add an Employee
+                    //Add an Employee Questions
                     const addEmployeeQuestions = [
                         {
                             name: 'firstName',
@@ -143,16 +144,14 @@ function askStartQuestion() {
                             choices: managers
                         }
                     ];
+                    //Add Employee to the Table
                     inquirer.prompt(addEmployeeQuestions).then((response) => {
-                        console.log(response);
                         db.query(`SELECT id FROM role WHERE title = '${response.employeeRole}';`, function (err, results) {
-                            console.log(results);
                             const roleID = results[0].id;
                             db.query(`SELECT id FROM employee WHERE first_name = '${response.employeeManager}';`, function (err, results) {
-                                console.log(results);
                                 const managerID = results[0].id;
                                 db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${response.firstName}', '${response.lastName}', '${roleID}', '${managerID}');`, function(err, results) {
-                                    console.log(results);
+                                    console.log("Success!", results);
                                     askStartQuestion();
                                 });
                             });
@@ -160,6 +159,8 @@ function askStartQuestion() {
                     });
                 });
             });
+        
+        //Update Employee function 
         }else if (response.startQuestion === 'Update Employee Role') {
             db.query(roleList, function (err, roleResponse) {
                 let roleTitles = roleResponse.map(function(roleResponseItem) {
@@ -167,20 +168,17 @@ function askStartQuestion() {
                         value: roleResponseItem.id,
                         name: roleResponseItem.title
                     }
-                    console.log('jobs', newRoleID)
                     return newRoleID;
                 });
-                console.log('jobsTitles', roleTitles)
                 db.query(allEmployeeFirstName, function (err, employeeResults) {
-                    console.log(employeeResults);
                     let updatingEmployee = employeeResults.map(function(row) {
                         let currentEmployee = {
                             value: row.id,
                             name: row.first_name
                         }
-                        console.log(`employee `, currentEmployee);
                         return currentEmployee;
                     })
+                    //Update Employee Questions
                     const updateEmployeeQuestions = [
                         {
                             name: 'updateEmployee',
@@ -195,10 +193,10 @@ function askStartQuestion() {
                             choices: roleTitles,
                         },
                     ];
+                    //Update Employee on the table
                     inquirer.prompt(updateEmployeeQuestions).then((response) => {
-                        console.log(response);
                         db.query(`UPDATE employee SET role_id = "${response.newRole}" WHERE id = ${response.updateEmployee};`, function(err, results) {
-                            console.log(results);
+                            console.log("Success!", results);
                             askStartQuestion();
                         });
                     });
@@ -211,9 +209,5 @@ function askStartQuestion() {
     });
 }
 
-
-//Add Employee function
-
-//Update Employee function
-
+//Call the starting question
 askStartQuestion();
